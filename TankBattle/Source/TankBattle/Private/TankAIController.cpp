8aae0db19cf064c67bf09951d00e287d2c630a7c
jgastelum <jacobdgastelum@gmail.com>
2019-03-20 22:06:11 -0700

    TankAIControllers can now find the PlayerPawn with GetPlayerTank()

--- TankBattle/Source/TankBattle/Private/TankAIController.cpp
+++ TankBattle/Source/TankBattle/Private/TankAIController.cpp
@@ -1,24 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
+#include "Engine/World.h"
 #include "TankBattle.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Not Possesed tank: %s"));
+		UE_LOG(LogTemp, Warning, TEXT("404 PlayerTank not found: %s"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("It works fam: %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("Found the fam: %s"), *(PlayerTank->GetName()));
 	}
 }
 
 ATank*ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }

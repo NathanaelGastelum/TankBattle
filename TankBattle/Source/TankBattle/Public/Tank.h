91adf9f00bc47d24adbd5d679d7fe91b61798ab7
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-04-08 19:40:01 -0700

    Implemented TankAimingComponent

--- TankBattle/Source/TankBattle/Public/Tank.h
+++ TankBattle/Source/TankBattle/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -25,9 +26,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 public:	
 	void AimAt(FVector Hitlocation);
 
-	
-	
 };

f540863c4c5d07013b9db9cd650f14b83f9f90cd
jgastelum <jacobdgastelum@gmail.com>
2019-04-25 13:27:14 -0700

    Implemented barrel elevating within clamped values.

--- TankBattle/Source/TankBattle/Private/TankBarrel.cpp
+++ TankBattle/Source/TankBattle/Private/TankBarrel.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBattle.h"
+#include "Engine/World.h"
 #include "TankBarrel.h"
 
 
@@ -8,4 +9,12 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move barrel the right amount this frame 
 	// given a max elevation speed, and the frame time
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+
+	auto ClampedElevation = FMath::Clamp<float>(RawNewElevation, MinDegreesElevation, MaxDegreesElevation);
+
+	SetRelativeRotation(FRotator(ClampedElevation, 0, 0));
 }

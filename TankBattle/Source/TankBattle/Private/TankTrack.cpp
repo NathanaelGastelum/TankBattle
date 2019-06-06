390d88c8d15d7622243ea61a11fcd108c23e5c5b
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-06-05 20:37:33 -0700

    Created TankTrack class and added throttle input

---
+++ TankBattle/Source/TankBattle/Private/TankTrack.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s calling throttle: %f"), *Name, Throttle);
+}
+
+//TODO clamp actual throttle value so player can't overly increase their speed

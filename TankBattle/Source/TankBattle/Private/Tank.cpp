491e3436f37f793020fff5c50a08d15a3baade28
NathanaelGastelum <nathanaelgastelum@gmail.com>
2019-08-16 11:12:03 -0700

    Refactor component architecture to blueprints

--- TankBattle/Source/TankBattle/Private/Tank.cpp
+++ TankBattle/Source/TankBattle/Private/Tank.cpp
@@ -16,7 +16,6 @@ ATank::ATank()
 
 	// No need to protect pointer as it is added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 
 }
 
--- TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
+++ TankBattle/Source/TankBattle/Private/TankMovementComponent.cpp
@@ -1,8 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return;}
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	//TODO: Prevent holding both manual throttles and fly by wire throttle from doubling your speed
 }
